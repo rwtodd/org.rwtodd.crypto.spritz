603b7cab71050c2f09ab531bb2e4f40160e6402c
Richard Todd <richard@waywardcode.com>
2016-05-23 20:56:38 -0500

    reorganized java version

--- java_version/build_jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# nothing fancy... I hate that the standard build tools (Ant, Maven, etc) are 
-# all so full of dependencies.  A simple tool is best, in my opinion.  So for
-# the moment, for utilities, I stick with a 2-line shell script. 
-
-javac -cp . com/waywardcode/crypto/*.java rwt/spritz/*.java 
-find rwt com -type f \! -name '*.java' | xargs jar cvfm spritz_cipher.jar Manifest.txt
-
