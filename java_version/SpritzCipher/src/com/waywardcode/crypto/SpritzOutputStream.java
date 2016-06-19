d349adfd2489364b48cafcccbe7ef9b8681ae0ad
Richard Todd <richard@waywardcode.com>
2016-06-18 21:01:08 -0500

    updated license information

--- java_version/SpritzCipher/src/com/waywardcode/crypto/SpritzOutputStream.java
+++ java_version/SpritzCipher/src/com/waywardcode/crypto/SpritzOutputStream.java
@@ -1,19 +1,8 @@
-// The Spritz Cipher
-// This implementation is copyright 2015 Richard Todd
-
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 2 of the License, or
-//   (at your option) any later version.
-// 
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-// 
-//   You should have received a copy of the GNU General Public License
-//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+/*
+ * Copyright Richard Todd. I put the code under the
+ * GPL v2.0.  See the LICENSE file in the repository.
+ * for more information.
+ */
 package com.waywardcode.crypto;
 
 
--- java_version/SpritzCipher/test/com/waywardcode/crypto/SpritzCipherTest.java
+++ java_version/SpritzCipher/test/com/waywardcode/crypto/SpritzCipherTest.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright Richard Todd. I put the code under the
+ * GPL v2.0.  See the LICENSE file in the repository.
+ * for more information.
  */
 package com.waywardcode.crypto;
 
